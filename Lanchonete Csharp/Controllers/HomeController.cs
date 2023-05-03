6116c62a1bdf6fd8a2f0985b9b0cfff0026b07cc
EduardoLoppes <mcfgonzalles@gmail.com>
2023-05-03 16:14:01 -0300

    Começo do projeto

---
+++ Lanchonete Csharp/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Lanchonete_Csharp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Lanchonete_Csharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Lanchonete Csharp/Views/Shared/carousel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lanchonete_Csharp.Views.Shared
+{
+    public class carouselModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
